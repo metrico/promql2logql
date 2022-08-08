28ef0042e5b87db16cb47285257bf773cff5ddd4
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-08-08 16:58:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 const Sqrl = require('squirrelly');
 const jsonic = require('jsonic');
-const template = '{{ it.name }}({{@each(it.args) => arg}}{"__name__"="{{arg.name}}"{{@if(arg.label_matchers !== null )}}{{@each(arg.label_matchers) => tag}}, "{{tag.name}}"="{{tag.value}}"{{/each}}{{/if}}} | unwrap_value[{{ arg.range }}]{{/each}}){{@if(it.aggregation !== false)}} by ({{it.aggregation.labels}}){{/if}}'
+const template = '{{ it.name }}({{@each(it.args) => arg}}{"__name__"="{{arg.name}}"{{@if(arg.label_matchers !== null )}}{{@each(arg.label_matchers) => tag}}, "{{tag.name}}"="{{tag.value}}"{{/each}}{{/if}}} | unwrap_value [{{ arg.range }}]{{/each}}){{@if(it.aggregation !== false)}} by ({{it.aggregation.labels}}){{/if}}'
 
 const convert = function(data){
   if (!data||data.length==0) return;
