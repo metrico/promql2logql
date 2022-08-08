fa2be7d5793ee6ebe14af1939a667ee8346d79f4
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-08-08 16:41:20 +0200

    Create testme.js

---
+++ testme.js
@@ -0,0 +1,29 @@
+var Sqrl = require('squirrelly');
+
+var data = {
+  "name": "rate",
+  "args": [
+    {
+      "name": "foo",
+      "label_matchers": [
+        {
+          "op": "Equal",
+          "name": "bar",
+          "value": "baz"
+        }
+      ],
+      "offset": false,
+      "range": "300s"
+    }
+  ],
+  "aggregation":{ "action":"By","labels":["x","y"]}
+};
+
+var template = '{{ it.name }}({{@each(it.args) => arg}}{"__name__"="{{arg.name}}"{{@if(arg.label_matchers !== null )}}{{@each(arg.label_matchers) => tag}}, "{{tag.name}}"="{{tag.value}}"{{/each}}{{/if}}}[{{ arg.range }}]{{/each}}){{@if(it.aggregation !== false)}} by ({{it.aggregation.labels}}){{/if}}'
+
+try {
+  var output = Sqrl.render(template, data)
+  console.log(output);
+} catch(e) {
+  console.log(e);
+}
